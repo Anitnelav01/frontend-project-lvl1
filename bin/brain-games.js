aefba7ea5bda75522205665fe74908ebd1caaa30
root <root@LAPTOP-4T1UH53D.localdomain>
2022-08-03 22:33:37 +0300

    third party libraries

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
+import { userName, item } from '../src/cli.js';
+
+
 console.log("Welcome to the Brain Games!");
+console.log(userName);
+console.log('Hello, ' + userName + '!');
