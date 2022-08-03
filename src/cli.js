0bbf92357579823b3934233fc1a215c5ddf77bcf
root <root@LAPTOP-4T1UH53D.localdomain>
2022-08-03 22:59:26 +0300

    commit

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
-var readlineSync = require('readline-sync');
 
-var userName = readlineSync.question('May I have your name? ');
+let readlineSync = require('readline-sync');
+
+let userName = readlineSync.question('May I have your name? ');
 
 export default userName;
 
