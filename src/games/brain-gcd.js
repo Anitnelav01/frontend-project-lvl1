14b8deb89cadac014b4f6fb0c5935a6f7723153e
root <root@LAPTOP-4T1UH53D.localdomain>
2022-08-24 18:57:22 +0300

    error correction 3

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -5,7 +5,7 @@ const description = 'Find the greatest common divisor of given numbers';
 const minRange = 1;
 const maxRange = 50;
 
-const getGcd = (x, y) = y === 0 ? x : getGcd(y, x % y);
+const getGcd = (x, y) => y === 0 ? x : getGcd(y, x % y);
 
 const generateResponse = () => {
   const number1 = getRandomNumber(minRange, maxRange);
