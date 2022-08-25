fd28912b21a654d042846103ee6ead685a687a6f
root <root@LAPTOP-4T1UH53D.localdomain>
2022-08-25 22:25:26 +0300

    error correction 3

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,13 +2,13 @@ import run from '../index.js';
 import { getRandomNumber, getRandomIndex } from '../utils.js';
 
 const description = 'What number is missing in the progression?';
-const length = 10;
+const limitLength  = 10;
 const minRange = 0;
 const maxRange = 50;
 const minStep = 2;
 const maxStep = 5;
 
-const createProgression = (start, step) => {
+const createProgression = (start, step, length) => {
   const progression = [];
 
   for (let i = 0; i < length; i += 1) {
@@ -19,9 +19,9 @@ const createProgression = (start, step) => {
 };
 
 const generateResponse = () => {
-  let start = getRandomNumber(minRange, maxRange);
+  const start = getRandomNumber(minRange, maxRange);
   const step = getRandomNumber(minStep, maxStep);
-  const progression = createProgression(start, step);
+  const progression = createProgression(start, step, limitLength );
   const hiddenNumberIndex = getRandomIndex(progression);
 
   const correctAnswer = String(progression[hiddenNumberIndex]);
